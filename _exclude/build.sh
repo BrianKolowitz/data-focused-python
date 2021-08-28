cf45a94a5aba2b97172794626a1168b376eba098
Brian Kolowitz <bkolowitz@gmail.com>
2021-08-28 12:02:22 -0400

    update search index

--- _exclude/build.sh
+++ _exclude/build.sh
@@ -1,4 +1,4 @@
 jupyter nbconvert --to markdown lectures/**/*.ipynb
-python3 _exclude/build-lecture-toc.py
-python3 _exclude/build-README.py
+python _exclude/build-lecture-toc.py
+python _exclude/build-README.py
 bundle exec just-the-docs rake search:init
